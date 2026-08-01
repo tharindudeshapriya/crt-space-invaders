7745e7b049f3c3b44125c56e99b6df2c7540db83
Tharindu Deshapriya <tmdeshapriya@gmail.com>
2026-08-01 20:46:59 +0530

    pm2 update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'crt-space-invaders',
+      name: 'space-defender-1984',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 3000',
+      cwd: '/var/www/app',
       instances: 'max',
       exec_mode: 'cluster',
-      env: {
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '800M',
+      env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,
-      },
-    },
-  ],
+        PORT: 3000
+      }
+    }
+  ]
 };
